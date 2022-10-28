20cb534921115ee1b97e2783e8d91277ad69adf8
unknown <robert_lu20@hotmail.com>
2022-10-27 23:41:29 -0500

    Added an .env.example

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,16 @@ import Movies from "./pages/leagues";
 
 export function middleware(request: NextRequest) {
     if (request.nextUrl.pathname.startsWith('/leauges')) {
-      return NextResponse.rewrite(new URL('/leagues', request.url))
+      return NextResponse.redirect(new URL('/leagues', request.url))
     }
 
     if (request.nextUrl.pathname.startsWith('/ligas')) {
         return NextResponse.rewrite(new URL('/leagues', request.url))
-      }
+    }
+
+    if (request.nextUrl.pathname.startsWith('/clubes')) {
+      return NextResponse.redirect(new URL('/leagues', request.url))
+    }
   
    
 }
